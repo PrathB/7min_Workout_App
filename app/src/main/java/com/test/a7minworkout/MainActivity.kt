c6feeb1d803ba4249f4bb8368e41d2040727943c
Pratham Batra <pratham11.batra@gmail.com>
2023-09-22 19:07:49 +0530

    Created home page UI

--- app/src/main/java/com/test/a7minworkout/MainActivity.kt
+++ app/src/main/java/com/test/a7minworkout/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.test.a7minworkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.test.a7minworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
