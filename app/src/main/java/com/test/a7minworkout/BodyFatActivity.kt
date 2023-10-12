e77f5684e1569cf5346fc95fba424708bdf879aa
Pratham Batra <pratham11.batra@gmail.com>
2023-10-12 11:41:25 +0530

    Input field now changes based on different radio buttons selected

--- app/src/main/java/com/test/a7minworkout/BodyFatActivity.kt
+++ app/src/main/java/com/test/a7minworkout/BodyFatActivity.kt
@@ -18,7 +18,7 @@ class BodyFatActivity : AppCompatActivity() {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
 
-        binding?.bodyFatActivityActionBar?.title = "CALCULATE BODY FAT %"
+        binding?.bodyFatActivityActionBar?.title = "CALCULATE BODY FAT"
 
         binding?.bodyFatActivityActionBar?.setNavigationOnClickListener {
             onBackPressed()
@@ -26,16 +26,68 @@ class BodyFatActivity : AppCompatActivity() {
 
         binding?.rbImperialUnits?.setOnClickListener {
 //            the metric input field is made invisible and imperial made visible
-            binding?.llMetricBodyFatInput?.visibility = View.INVISIBLE
-            binding?.llImperialBodyFatInput?.visibility = View.VISIBLE
-            binding?.llBmiValue?.visibility = View.INVISIBLE
+            if(binding?.rbMale?.isChecked == true) {
+                binding?.llMetricBodyFatInput?.visibility = View.INVISIBLE
+                binding?.llImperialBodyFatInput?.visibility = View.VISIBLE
+
+                binding?.textInputLayoutHipImperial?.visibility = View.INVISIBLE
+
+                binding?.llBodyFatValue?.visibility = View.INVISIBLE
+            }
+            else{
+                binding?.llMetricBodyFatInput?.visibility = View.INVISIBLE
+                binding?.llImperialBodyFatInput?.visibility = View.VISIBLE
+
+                binding?.textInputLayoutHipMetric?.visibility = View.INVISIBLE
+                binding?.textInputLayoutHipImperial?.visibility = View.VISIBLE
+
+                binding?.llBodyFatValue?.visibility = View.INVISIBLE
+            }
         }
 
         binding?.rbMetricUnits?.setOnClickListener {
 //            the imperial input field is made invisible and metric made visible
-            binding?.llMetricBodyFatInput?.visibility = View.VISIBLE
-            binding?.llImperialBodyFatInput?.visibility = View.INVISIBLE
-            binding?.llBmiValue?.visibility = View.INVISIBLE
+            if(binding?.rbMale?.isChecked == true) {
+                binding?.llMetricBodyFatInput?.visibility = View.VISIBLE
+                binding?.llImperialBodyFatInput?.visibility = View.INVISIBLE
+
+                binding?.textInputLayoutHipMetric?.visibility = View.INVISIBLE
+
+                binding?.llBodyFatValue?.visibility = View.INVISIBLE
+            }
+            else{
+                binding?.llMetricBodyFatInput?.visibility = View.VISIBLE
+                binding?.llImperialBodyFatInput?.visibility = View.INVISIBLE
+
+                binding?.textInputLayoutHipMetric?.visibility = View.VISIBLE
+                binding?.textInputLayoutHipImperial?.visibility = View.INVISIBLE
+
+                binding?.llBodyFatValue?.visibility = View.INVISIBLE
+            }
+        }
+
+        binding?.rbFemale?.setOnClickListener {
+//            the metric/imperial hip measurement input is made visible according to unit selected
+            if(binding?.rbMetricUnits?.isChecked == true){
+                binding?.textInputLayoutHipMetric?.visibility = View.VISIBLE
+                binding?.llBodyFatValue?.visibility = View.INVISIBLE
+            }
+            else{
+                binding?.textInputLayoutHipImperial?.visibility = View.VISIBLE
+                binding?.llBodyFatValue?.visibility = View.INVISIBLE
+            }
+        }
+
+        binding?.rbMale?.setOnClickListener {
+//            the metric/imperial hip measurement input is made invisible according to unit selected
+            if(binding?.rbMetricUnits?.isChecked == true){
+                binding?.textInputLayoutHipMetric?.visibility = View.INVISIBLE
+                binding?.llBodyFatValue?.visibility = View.INVISIBLE
+            }
+            else{
+                binding?.textInputLayoutHipImperial?.visibility = View.INVISIBLE
+                binding?.llBodyFatValue?.visibility = View.INVISIBLE
+            }
         }
     }
 }
